875b5e38cb092ba945512e2fbbfc9b95ec6f0edd
Mohammad.Hussain2 <mohammad.hussain2@MohammadHussain2s-MacBook-Pro.local>
2022-08-11 17:13:21 +0530

    code updated

--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import '../../../assets/styles/style.css'
 export const Checkbox = () => {
   return (
     <>
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
+
+import { Thing } from '../src/Thing'
 
 describe('Thing', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const {getByText} = render(<Thing>Hello</Thing>)
+    const helloNode = getByText('Hello')
+    expect(helloNode).toBeInTheDocument()
   });
 });
+
+
+// import { render, screen } from '@testing-library/react'
+// import App from 'src/App';
+
+// describe('App', () => {
+//     test('renders home page', () => {
+//         render(<App />);
+//         const text = screen.getByText(/Running Finance /i);
+//         expect(text).toBeInTheDocument();
+//     });
+// });
